9d867ed930f10935261726f2923a98507274fa75
raineryue <it_jinlei.yue@163.com>
2016-03-18 17:59:07 +0800

    Initial Commit

---
+++ 20160318004-HitTest-Exercise1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160318004-HitTest-Exercise1
+//
+//  Created by Rainer on 16/3/18.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 20160318004-HitTest-Exercise1Tests/_0160318004_HitTest_Exercise1Tests.m
@@ -0,0 +1,39 @@
+//
+//  _0160318004_HitTest_Exercise1Tests.m
+//  20160318004-HitTest-Exercise1Tests
+//
+//  Created by Rainer on 16/3/18.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0160318004_HitTest_Exercise1Tests : XCTestCase
+
+@end
+
+@implementation _0160318004_HitTest_Exercise1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
